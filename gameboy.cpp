41c337eaf5eb7193b5528f71472c9ca9ef2af66a
Amey K <ameykhairnar99@gmail.com>
2024-10-19 18:35:14 -0400

    Added Step 3.A

--- gameboy.cpp
+++ gameboy.cpp
@@ -19,15 +19,134 @@ char* rom;
 int romSize;
 
 unsigned char graphicsRAM[8192];
+unsigned char workingRAM[0x2000];
+unsigned char page0RAM[0x80];
 int palette[4];
 int tileSet, tileMap, scrollX, scrollY;
 int Screen[SCREEN_W][SCREEN_H];
+int H_BLANK = 0, V_BLANK = 1, SPRITE = 2, VRAM = 3;
+int line = 0, cmpLine = 0, videoState = 0, keyboardColumn = 0, horizontal = 0;
+int gpuMode = H_BLANK;
+int romOffset = 0x4000;
+long totalInstructions = 0;
 
-unsigned char memoryRead(int address) {
-  return rom[address];
+/* -------------------------------------------------------------------------- */
+// Function Definitions
+void setControlByte(unsigned char b);
+void setPalette(unsigned char b);
+unsigned char getVideoState();
+
+unsigned char memoryRead(int address);
+void memoryWrite(int address, unsigned char value);
+
+void readScreen();
+void renderScreen();
+
+int main(int argc, char* argv[]) {
+  // setup(argc, argv)
+
+  // --------------------------------------------------------------------------
+  // Step 1
+  ifstream romFile("testrom.gb", ios::in | ios::binary | ios::ate);
+  streampos size = romFile.tellg();
+
+  rom = new char[size];
+  romSize = size;
+  romFile.seekg(0, ios::beg);
+  romFile.read(rom, size);
+  romFile.close();
+
+  z80 = new Z80(memoryRead, memoryWrite);
+  z80->reset();
+
+  // Step 1: No longer used
+  //   z80->PC = 0;
+
+  while (!z80->halted) {
+    z80->doInstruction();
+    printf("PC=%x\tA=%d\tB=%d\n", z80->PC, z80->A, z80->B);
+  }
+
+  // --------------------------------------------------------------------------
+  // Step 2
+  ifstream vidfile("screendump.txt", ios::in);
+
+  // Read first 8192 integers into graphics RAM
+  for (int i = 0; i < 8192; i++) {
+    int n;
+    vidfile >> n;
+    graphicsRAM[i] = (unsigned char)n;
+  }
+
+  // Read rest of the variables
+  vidfile >> tileSet;
+  vidfile >> tileMap;
+  vidfile >> scrollX;
+  vidfile >> scrollY;
+  vidfile >> palette[0];
+  vidfile >> palette[1];
+  vidfile >> palette[2];
+  vidfile >> palette[3];
+
+  renderScreen();
+
+  return 0;
 }
 
-void memoryWrite(int address, unsigned char value) {}
+unsigned char memoryRead(int address) {
+  if (0 <= address && address <= 0x3fff)
+    return rom[address];
+  else if (0x4000 <= address && address <= 0x7fff)
+    return rom[romOffset + address % 0x4000];
+  else if (0x8000 <= address && address < 0x9fff)
+    return graphicsRAM[address % 0x2000];
+  else if (0xc000 <= address && address < 0xdfff)
+    return workingRAM[address % 0x2000];
+  else if (0xff80 <= address && address < 0xffff)
+    return page0RAM[address % 0x80];
+  else if (address == 0xff00)
+    return 0xf;
+  else if (address == 0xff41)
+    return getVideoState();
+  else if (address == 0xff42)
+    return scrollY;
+  else if (address == 0xff43)
+    return scrollX;
+  else if (address == 0xff44)
+    return line;
+  else if (address == 0xff45)
+    return cmpLine;
+
+  // For anything else, we return 0
+  return 0;
+}
+
+void memoryWrite(int address, unsigned char value) {
+  if (0 <= address && address <= 0x7fff)
+    return;
+  else if (0x8000 <= address && address < 0x9fff)
+    graphicsRAM[address % 0x2000] = value;
+  else if (0xc000 <= address && address < 0xdfff)
+    workingRAM[address % 0x2000] = value;
+  else if (0xff80 <= address && address < 0xffff)
+    page0RAM[address % 0x80] = value;
+  else if (address == 0xff00)
+    keyboardColumn = value;
+  else if (address == 0xff40)
+    setControlByte(value);
+  else if (address == 0xff41)
+    videoState = value;
+  else if (address == 0xff42)
+    scrollY = value;
+  else if (address == 0xff43)
+    scrollX = value;
+  else if (address == 0xff44)
+    line = value;
+  else if (address == 0xff45)
+    cmpLine = value;
+  else if (address == 0xff47)
+    setPalette(value);
+}
 
 /*
 Sets colors on 4 grade palette and reads into Screen
@@ -88,53 +207,27 @@ void renderScreen() {
   }
 }
 
-int main(int argc, char* argv[]) {
-  // setup(argc, argv)
+void setControlByte(unsigned char b) {
+  tileMap = (b & 8) != 0 ? 1 : 0;
+  tileSet = (b & 16) != 0 ? 1 : 0;
+}
 
-  // --------------------------------------------------------------------------
-  // Step 1
-  ifstream romFile("testrom.gb", ios::in | ios::binary | ios::ate);
-  streampos size = romFile.tellg();
+void setPalette(unsigned char b) {
+  palette[0] = b & 3;
+  palette[1] = (b >> 2) & 3;
+  palette[2] = (b >> 4) & 3;
+  palette[3] = (b >> 6) & 3;
+}
 
-  rom = new char[size];
-  romSize = size;
-  romFile.seekg(0, ios::beg);
-  romFile.read(rom, size);
-  romFile.close();
-
-  z80 = new Z80(memoryRead, memoryWrite);
-  z80->reset();
-
-  // Step 1: No longer used
-  //   z80->PC = 0;
-
-  while (!z80->halted) {
-    z80->doInstruction();
-    printf("PC=%x\tA=%d\tB=%d\n", z80->PC, z80->A, z80->B);
-  }
-
-  // --------------------------------------------------------------------------
-  // Step 2
-  ifstream vidfile("screendump.txt", ios::in);
-
-  // Read first 8192 integers into graphics RAM
-  for (int i = 0; i < 8192; i++) {
-    int n;
-    vidfile >> n;
-    graphicsRAM[i] = (unsigned char)n;
-  }
-
-  // Read rest of the variables
-  vidfile >> tileSet;
-  vidfile >> tileMap;
-  vidfile >> scrollX;
-  vidfile >> scrollY;
-  vidfile >> palette[0];
-  vidfile >> palette[1];
-  vidfile >> palette[2];
-  vidfile >> palette[3];
-
-  renderScreen();
-
-  return 0;
+unsigned char getVideoState() {
+  int by = 0;
+  if (line == cmpLine)
+    by |= 4;
+  if (gpuMode == V_BLANK)
+    by |= 1;
+  if (gpuMode == SPRITE)
+    by |= 2;
+  if (gpuMode == VRAM)
+    by |= 3;
+  return (unsigned char)((by | (videoState & 0xf8)) & 0xff);
 }
